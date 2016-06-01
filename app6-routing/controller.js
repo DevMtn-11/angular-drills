6078175bbad161a67c62a1e1b359553bf0ea5eca
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-05-31 20:10:04 -0600

    reset app6

--- app6-routing/controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('routingApp')
-  .controller('controller', function($scope, service) {
-
-      service.getStarWarsData()
-        .then(function(response) {
-          $scope.characters = response;
-        });
-
-  });
